4db783c2709a88d961d34b510385ef4c25aa6a17
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-02 15:44:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (86.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    
-    int dp[1001];
-    
-    int helper(vector<int>& nums, int index){
-        if(index >= nums.size()){
-            return 0;
-        }
-        
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        
-        int rob = nums[index] + helper(nums, index + 2);
-        int notRob = helper(nums, index + 1);
-        
-        return dp[index] = max(rob, notRob);
-    }
-    
+    int dp[10001];
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return helper(nums,0);
+        int n = nums.size();
+        
+        if(n == 1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        
+        for(int i=2; i<n; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
