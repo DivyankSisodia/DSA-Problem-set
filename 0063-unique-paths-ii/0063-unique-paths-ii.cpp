c7d2b66e0d54b6e77364292f63d74b51e90efa3c
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-02 22:27:52 +0530

    Time: 8 ms (11.38%), Space: 7.9 MB (48.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,18 +2,43 @@ class Solution {
 public:
     
     vector<vector<int>> dp;
-    int fun(int x, int y, vector<vector<int>>& grid){
-        if(grid[x][y]==1) return 0;
-        if(x==0 && y==0) return 1;
-        if(dp[x][y]!=-1) return dp[x][y];
-        int ans = 0;
-        if(x>0) ans += fun(x-1, y, grid);
-        if(y>0) ans += fun(x, y-1, grid);
-        return dp[x][y] = ans;
-    }
+    // int fun(int x, int y, vector<vector<int>>& grid){
+    //     if(grid[x][y]==1) return 0;
+    //     if(x==0 && y==0) return 1;
+    //     if(dp[x][y]!=-1) return dp[x][y];
+    //     int ans = 0;
+    //     if(x>0) ans += fun(x-1, y, grid);
+    //     if(y>0) ans += fun(x, y-1, grid);
+    //     return dp[x][y] = ans;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();   
         dp.resize(m, vector<int>(n, -1));
-        return fun(m-1, n-1, grid);
+        // return fun(m-1, n-1, grid);
+        
+        for(int i=0 ;i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
