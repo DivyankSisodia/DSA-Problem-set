8dbb9073d053f7be4b9665469358b808831fc858
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-08 18:51:02 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (69.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-       int dp[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0||j==0)
-                    dp[i][j]=1;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+    int dp[101][101];
+    int solve(int i, int j, int m, int n){
+        if(i == 0 &&  j == 0){
+            return 1;
         }
-        return dp[m-1][n-1];
+        if( i<0 || j<0 || i>m || j>n){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int up = solve(i-1,j,m,n);
+        int left = solve(i,j-1,m,n);
+        
+        return dp[i][j] = up+left;
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(m-1,n-1,m,n);
     }
 };
