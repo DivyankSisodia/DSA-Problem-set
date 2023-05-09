47acbfdabad50845650bbe756f803e81e3c845d2
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-09 19:40:30 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMinInsertions(string s){
+        // code here 
+        int n = s.size();
+        string t = s;
+        reverse(t.begin(), t.end());
+        
+        int dp[n+1][n+1];
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+                else if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        int lcs = dp[n][n];
+        
+        return n-lcs;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.findMinInsertions(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
