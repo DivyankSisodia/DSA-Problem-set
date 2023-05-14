74ef544c283c7fbe2af5d03765e9f61ddad83999
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-14 12:24:34 +0530

    Time: 9 ms (89.08%), Space: 10.3 MB (98.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,43 @@
+            // TABULATION
+
+
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         for(int ind=n-1;ind>=0;ind--){
+//             for(int prev=ind-1;prev>=-1;prev--){
+//                 int notTake=0+ dp[ind+1][prev+1];
+//                 int take=0;
+//                 if(prev==-1 || nums[ind]>nums[prev]){
+//                     take=1+dp[ind+1][ind+1];
+//                 }
+//                  dp[ind][prev+1]= max(take,notTake);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+            // BINARY SEARCH
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-                int notTake=0+ dp[ind+1][prev+1];
-                int take=0;
-                if(prev==-1 || nums[ind]>nums[prev]){
-                    take=1+dp[ind+1][ind+1];
-                }
-                 dp[ind][prev+1]= max(take,notTake);
+        vector<int> ans;
+        int len = 1;
+        ans.push_back(nums[0]);
+        for(int i=1; i<n; i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int x = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[x] = nums[i];
             }
         }
-        return dp[0][0];
+        return len;
     }
 };
