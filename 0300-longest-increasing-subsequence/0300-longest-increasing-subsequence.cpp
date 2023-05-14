2533c520f8cfe1b74eecf87608d2a2d24b725b92
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-14 12:13:48 +0530

    Time: 646 ms (28.86%), Space: 292.3 MB (9.08%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    
-    int solve(vector<int>&nums,int n, int idx, int pre,vector<vector<int>>&dp){
-        if(idx == n){
-            return 0;
-        }
-        if(dp[idx][pre+1] != -1){
-            return dp[idx][pre+1];
-        }
-        int notTake = solve(nums,n,idx+1,pre,dp);
-        int take = 0;
-        if(pre == -1 || nums[idx] > nums[pre]){
-            take = 1 + solve(nums,n,idx+1,idx,dp);
-        }
-        return dp[idx][pre+1] = max(take,notTake);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+2,-1));
-        return solve(nums,n,0,-1,dp);
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int notTake=0+ dp[ind+1][prev+1];
+                int take=0;
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    take=1+dp[ind+1][ind+1];
+                }
+                 dp[ind][prev+1]= max(take,notTake);
+            }
+        }
+        return dp[0][0];
     }
 };
