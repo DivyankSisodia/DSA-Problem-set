0005957afee62d286e8df5b77e27e5dd3354c924
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-21 11:23:24 +0530

    Time: 15 ms (73.57%), Space: 8.9 MB (42.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char,int> mp;
-        int length = 0;
-        int maxLen = 0;
-        int j = 0;
+//         map<char,int> mp;
+//         int length = 0;
+//         int maxLen = 0;
+//         int j = 0;
         
-        for(int i=0; i<s.size(); i++){
-            mp[s[i]]++;
-            length++;
-            while(mp[s[i]] > 1){
-                mp[s[j]]--;
+//         for(int i=0; i<s.size(); i++){
+//             mp[s[i]]++;
+//             length++;
+//             while(mp[s[i]] > 1){
+//                 mp[s[j]]--;
+//                 j++;
+//                 length--;
+//             }
+//             maxLen = max(maxLen,length);
+//         }
+//         return maxLen;
+        
+        int i=0;
+        int j=0;
+        int ans = 0;
+        int n = s.size();
+        unordered_map<char,int> mp;
+        
+        while(j<n){
+            mp[s[j]]++;
+            
+            // waise yeh condn kbhi hit kregi hi nhi
+            if(mp.size() > j-i+1){
                 j++;
-                length--;
             }
-            maxLen = max(maxLen,length);
+            
+            
+            if(mp.size() == j-i+1){
+                ans = max(ans,j-i+1);
+                j++;
+            }
+            else{
+                while(mp.size() < j-i+1){
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0){
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
         }
-        return maxLen;
+        return ans;
     }
 };
