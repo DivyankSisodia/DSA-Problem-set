2c876cf2e240a1788c6df62404324fc76e8e6a29
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-05-22 10:55:04 +0530

    Time: 214 ms (56.89%), Space: 53.3 MB (68.15%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,43 +11,33 @@
 class Solution {
 public:
     
-     ListNode* mergelist(ListNode *l1, ListNode *l2)
+     ListNode* merge(ListNode* first, ListNode* second)
     {
-        ListNode *ptr = new ListNode(0);
-        ListNode *curr = ptr;
+        ListNode* answer = NULL; // define answer to null
         
-        while(l1 != NULL && l2 != NULL)
+        if(first == NULL) // if first is null, then what to merge...nothing
         {
-            if(l1->val <= l2->val)
-            {
-                curr -> next = l1;
-                l1 = l1 -> next;
-            }
-            else
-            {
-                curr -> next = l2;
-                l2 = l2 -> next;
-            }
-        
-        curr = curr ->next;
-        
+            return second; // return second
         }
         
-        //for unqual length linked list
-        
-        if(l1 != NULL)
+        if(second == NULL) // if second is null, then what to merge...nothing
         {
-            curr -> next = l1;
-            l1 = l1->next;
+            return first; // return first
         }
         
-        if(l2 != NULL)
+        // if value of first is less than value of second,then give answer to first
+        if(first -> val <= second -> val) 
         {
-            curr -> next = l2;
-            l2 = l2 ->next;
+            answer = first;
+            answer -> next = merge(first -> next, second); // and again call merge for answer's next
+        }
+        else // else give answer to second
+        {
+            answer = second;
+            answer -> next = merge(first, second -> next); // and again call merge for answer's next
         }
         
-        return ptr->next;
+        return answer; // finally return answer
     }
     
     ListNode* sortList(ListNode* head) {
@@ -68,6 +58,6 @@ public:
         ListNode* l1 = sortList(head);
         ListNode* l2 = sortList(slow);
         
-        return mergelist(l1,l2);
+        return merge(l1,l2);
     }
 };
