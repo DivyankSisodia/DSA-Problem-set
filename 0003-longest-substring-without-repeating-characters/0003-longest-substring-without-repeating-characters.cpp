94b6cd07997d50b5fd9a2846c37729ffa5542526
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-06-18 10:14:56 +0530

    Time: 32 ms (36.48%), Space: 10.1 MB (36.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-//         map<char,int> mp;
-//         int length = 0;
-//         int maxLen = 0;
-//         int j = 0;
         
-//         for(int i=0; i<s.size(); i++){
-//             mp[s[i]]++;
-//             length++;
-//             while(mp[s[i]] > 1){
-//                 mp[s[j]]--;
-//                 j++;
-//                 length--;
-//             }
-//             maxLen = max(maxLen,length);
-//         }
-//         return maxLen;
+        int n = s.size();
         
+        map<char, int> mp;
         int i=0;
         int j=0;
         int ans = 0;
-        int n = s.size();
-        unordered_map<char,int> mp;
         
         while(j<n){
             mp[s[j]]++;
             
-            // waise yeh condn kbhi hit kregi hi nhi
-            if(mp.size() > j-i+1){
-                j++;
-            }
-            
-            
             if(mp.size() == j-i+1){
-                ans = max(ans,j-i+1);
+                ans = max(ans, j-i+1);
                 j++;
             }
+            
             else{
+                // mp ke size se zyda hi traverse kr liya meine string ko , mtlb koi toh repeat ho gya 
+                // tbhi mp ka size km h
                 while(mp.size() < j-i+1){
                     mp[s[i]]--;
                     if(mp[s[i]] == 0){
@@ -46,7 +28,7 @@ public:
                     i++;
                 }
                 j++;
-            }
+            } 
         }
         return ans;
     }
