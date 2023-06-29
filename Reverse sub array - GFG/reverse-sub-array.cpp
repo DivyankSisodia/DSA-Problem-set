54bc4276735bc2383b166fa776b15067b266c19e
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-06-29 15:14:07 +0530

    Added solution - LeetHub

---
+++ Reverse sub array - GFG/reverse-sub-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void reverseSubArray(int *arr, int n, int l, int r) {
+	    // code here
+	    int temp;
+        int start = l-1;
+        int end = r-1;
+        
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> l >> r;
+        Solution ob;
+        ob.reverseSubArray(arr, n, l, r);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
