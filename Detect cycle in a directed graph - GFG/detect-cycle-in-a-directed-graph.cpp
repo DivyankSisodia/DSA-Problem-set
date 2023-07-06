1a0419953565f6add3adcfea10012de37ff815b5
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-07-06 10:29:51 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -7,38 +7,74 @@ class Solution {
   public:
     // Function to detect cycle in a directed graph.
     
-    bool dfs(int node, vector<int>adj[], int vis[], int pathvis[]){
-        vis[node] = 1;
-        pathvis[node] = 1;
+    // bool dfs(int node, vector<int>adj[], int vis[], int pathvis[]){
+    //     vis[node] = 1;
+    //     pathvis[node] = 1;
         
         
-        for(auto it: adj[node]){
-            if(vis[it]!=1){
-                if(dfs(it,adj,vis,pathvis) == true){
-                    return true;
-                }
-            }
-            else if(pathvis[it] == 1){
-                return true;
-            }
-        }
-        pathvis[node] = 0;
-        return false;
-    }
+    //     for(auto it: adj[node]){
+    //         if(vis[it]!=1){
+    //             if(dfs(it,adj,vis,pathvis) == true){
+    //                 return true;
+    //             }
+    //         }
+    //         else if(pathvis[it] == 1){
+    //             return true;
+    //         }
+    //     }
+    //     pathvis[node] = 0;
+    //     return false;
+    // }
     
     bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        int vis[V] = {0};
-        int pathvis[V] = {0};
         
+        int indegree[V] = {0};
         for(int i=0; i<V; i++){
-            if(vis[i] != 1){
-                if(dfs(i,adj, vis, pathvis) == true){
-                    return true;
-                }
+            for(auto it: adj[i]){
+                indegree[it]++;
             }
         }
-        return false;
+        
+        queue<int> q;
+        
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        int cnt = 0;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            
+            for(auto it: adj[node]){
+                 indegree[it]--;
+                 if(indegree[it] == 0){
+                     q.push(it);
+                 }
+            }
+        }
+        // cout<<cnt<<endl;
+        if(cnt == V){
+            return false;
+        }
+        return true;
+        
+    //     // code here
+    //     int vis[V] = {0};
+    //     int pathvis[V] = {0};
+        
+    //     for(int i=0; i<V; i++){
+    //         if(vis[i] != 1){
+    //             if(dfs(i,adj, vis, pathvis) == true){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+        // return false;
     }
 };
 
