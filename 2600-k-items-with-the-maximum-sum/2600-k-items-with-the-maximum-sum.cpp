5c486992c72a9f4a488b403851dda2fcea3bb03a
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-07-20 21:22:14 +0530

    Time: 4 ms (59.85%), Space: 6.6 MB (9.38%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     int kItemsWithMaximumSum(int one, int zero, int negone, int k) {
-        vector<int> ans(one+zero+negone);
-        for(int i=0; i<ans.size(); i++){
-            if(one > 0){
-                ans[i] = 1;
-                one--;
-            }
-            else if( zero > 0){
-                ans[i] = 0;
-                zero--;
-            }
-            else{
-                ans[i] = -1;
-                negone--;
-            }
-        }
+//         vector<int> ans(one+zero+negone);
+//         for(int i=0; i<ans.size(); i++){
+//             if(one > 0){
+//                 ans[i] = 1;
+//                 one--;
+//             }
+//             else if( zero > 0){
+//                 ans[i] = 0;
+//                 zero--;
+//             }
+//             else{
+//                 ans[i] = -1;
+//                 negone--;
+//             }
+//         }
         
-        // for(auto it: ans){
-        //     cout<<it<<" "<<endl;
-        // }
-        int sum = 0;
-        for(int i=0; i<k; i++){
-            sum = sum + ans[i];
-        }
-        return sum;
+//         // for(auto it: ans){
+//         //     cout<<it<<" "<<endl;
+//         // }
+//         int sum = 0;
+//         for(int i=0; i<k; i++){
+//             sum = sum + ans[i];
+//         }
+//         return sum;
+        priority_queue<int> pq;
+        while(one--) pq.push(1);
+        while(zero--) pq.push(0);
+        while(negone--) pq.push(-1);
+        
+        int ans = 0;
+        while(k--) ans += pq.top(), pq.pop();
+        return ans;
     }
 };
