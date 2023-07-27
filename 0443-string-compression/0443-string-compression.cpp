0b29d549e9aac7719972229105b7c4dca804ddf4
Divyank Sisodia <78336066+DivyankSisodia@users.noreply.github.com>
2023-07-27 14:56:07 +0530

    Time: 15 ms (7.44%), Space: 19.6 MB (7.16%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compress(vector<char>& nums) {
+        int n = nums.size();
+        int cnt = 1;
+        string ans;
+        
+        for(int i=0; i<n; i++){
+            while(i<n-1 && nums[i+1] == nums[i]){
+                cnt++;
+                i++;
+            }
+            
+            ans = ans + nums[i];
+            
+            if(cnt == 1){
+                continue;
+            }
+            else{
+                ans = ans + to_string(cnt);
+            }
+            cnt = 1;
+        }
+        
+        nums.clear();
+        
+        for(int i=0; i<ans.size(); i++){
+            nums.push_back(ans[i]);
+        }
+        return ans.size();
+    }
+};
